1a61f26549b6814c1f49be2d89904126699caafc
BALLOON | FU-SEN <balloonakafusen@gmail.com>
2021-02-20 19:19:07 +0900

    🐍＜MIT ライセンス入れるのを忘れてたの～

--- chrcode.py
+++ chrcode.py
@@ -1,3 +1,7 @@
+# Character code list | IchigoLatte Python
+# Copyright (c) 2021 Keiichi Shiga (BALLOON | FU-SEN)
+# The MIT License (MIT) - https://mit.balloon.net.eu.org/#2021
+
 # Character code list
 c=0
 cls()
